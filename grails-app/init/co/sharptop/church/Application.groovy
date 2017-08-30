825c2b6da4520730610c37c8a0a6d216926bbddb
Ryan Heathcote <rheathcote@sharptop.co>
2017-08-29 20:50:54 -0400

    Add cache refreshing every 10 minutes.

--- grails-app/init/co/sharptop/church/Application.groovy
+++ grails-app/init/co/sharptop/church/Application.groovy
@@ -9,7 +9,9 @@ package co.sharptop.church
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
--- grails-app/services/co/sharptop/church/RssPostService.groovy
+++ grails-app/services/co/sharptop/church/RssPostService.groovy
@@ -1,10 +1,14 @@
 package co.sharptop.church
 
 import grails.transaction.Transactional
+import org.springframework.scheduling.annotation.Scheduled
 
 @Transactional
 class RssPostService {
 
+    boolean lazyInit = false
+
+    ContentfulService contentfulService
     Map cache = [:]
 
     def fetch(PostGroup postGroup) {
@@ -13,9 +17,24 @@ class RssPostService {
         }
 
         if (!cache[postGroup.id]) {
-            cache[postGroup.id] = new RssUtil(postGroup.rssMetadata, "rssPost-${postGroup.id}").createRssPosts()
+            refreshFeed(postGroup)
         }
 
         return cache[postGroup.id]
     }
+
+    @Scheduled(fixedDelay=600000l, initialDelay = 30000l)
+    void refreshFeeds() {
+        log.info "Refreshing RSS Feeds"
+        contentfulService.fetchEntries(PostGroup).findAll {
+            it.rssMetadata
+        }.each { PostGroup postGroup ->
+            refreshFeed(postGroup)
+        }
+    }
+
+    void refreshFeed(PostGroup postGroup) {
+        cache[postGroup.id] = new RssUtil(postGroup.rssMetadata, "rssPost-${postGroup.id}").createRssPosts()
+    }
+
 }
