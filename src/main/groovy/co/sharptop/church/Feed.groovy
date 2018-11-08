89a21f44547388464608457312257a4e0d88a701
Brandon Tassone <brandontassone@gmail.com>
2018-11-08 04:52:04 -0500

    Removed feed items that are now in settings

--- src/main/groovy/co/sharptop/church/Feed.groovy
+++ src/main/groovy/co/sharptop/church/Feed.groovy
@@ -7,12 +7,7 @@ class Feed {
     List<Event> events
     List<PostGroup> postGroups
     Sermon sermon
-    String givingURL
-    Link liveStreamLink
     List<Song> songs
     Boolean hasMinistryGroups
     Boolean hasPrayerRequests
-    String prayerTimeImageURL
-    String eventsImageURL
-
 }
